62829b3ae66f581dfa0bf729c9a23111916bc07e
gabygimenes <gabriela.gimenes@edu.senai.br>
2026-09-10 10:44:40 -0300

    04

---
+++ 04-destruturing-array.js
@@ -0,0 +1,26 @@
+//4. DESESTRUTURAÇÃO (DESTRUCTURING) de arrays
+//============================================
+
+//4.1 Desestruturação básica de array (baseada na POSIÇÃO dos elementos)
+const [primeiraCor, segundaCor, terceiraCor] = cores;
+
+console.log ("Primeira cor:", primeiraCor);
+console.log ("Segunda cor:", segundaCor);
+console.log ("Terceira cor:", terceiraCor);
+
+//4.2 Comparando Objetos vs Arrays:
+// - Objeto: o nome da propriedade importa {nome, idade}
+// - Array: a ordem/posição importa [nome, idade]
+
+//Exemplo com Objeto:
+const pessoaObjeto = {
+    nome:"Carlos",
+    idade: 20
+};
+const { nome, idade } = pessoaObjeto;
+console.log(`Objeto -> Nome: ${nome}, Idade: ${idade}`);
+
+//Exemplo com Array
+const pessoaArray = ["Carlos",20];
+const [nomeAluno, idadeAluno] = pessoaArray;
+console.log(`Array -> Nome: ${nomeAluno}, Idade: ${idadeAluno}`);
