fe51a279cad2a546153b8c32663ed4dd14802126
gabygimenes <gabriela.gimenes@edu.senai.br>
2026-09-10 09:31:41 -0300

    01

---
+++ 01-arrow-functions.js
@@ -0,0 +1,29 @@
+//1.ARROW FUNCTIONS
+//==============================================
+ 
+//1.1 Arrow Function básica com bloco e return
+const saudacao = (nome) => {
+    return`Olá, ${nome}!`; 
+};
+console.log(saudacao("João")); //Olá, João!
+
+//1.2 Versão com retorno implícito (uma única linha)
+const saudacaoCurta = (nome) => `Olá, ${nome}!`;
+
+console.log(saudacaoCurta("Maria")); //Olá, Maria!
+
+//1.3 Arrow Function com cálculo
+const multiplicar = (a,b) => a * b;
+
+console.log("Resultado da multiplicação", multiplicar(4,5)); //20
+
+//1.4 Arrow Function com estrutura condicional (if / else)
+const verificarNota = (nota) => {
+    if(nota >= 7) {
+        return "Aprovado";
+    }
+    return "Rec";
+}
+
+console.log("Status do aluno (nota 8):", verificarNota(8)); //A
+console.log("Status do aluno (nota 5):", verificarNota(5)); //R
